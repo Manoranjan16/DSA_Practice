7bfaf8c2aa227bb09e3da22e2b510e1a4eee53a5
Manoranjan16 <pgul7674@gmail.com>
2026-07-31 21:06:57 +0530

    Updated the file

--- 2_median_value.py
+++ 2_median_value.py
@@ -7,7 +7,7 @@ l1 = [1,2]
 l2 = [3,4]
 
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays(self, nums1, nums2):
         merged = (nums1 + nums2)
         n = len(merged)
         
@@ -15,7 +15,7 @@ class Solution:
             return merged[n // 2]
         else:
             mid1, mid2 = merged[n // 2 - 1], merged[n // 2]
-            return (mid1 + mid2) / 2
+            return (mid1 + mid2) / 2.0
 
 s = Solution()
 print(s.findMedianSortedArrays(l1,l2))
