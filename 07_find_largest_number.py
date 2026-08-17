c05608c1d9292605b7a858c032c2eccf947f2c2c
Manoranjan16 <pgul7674@gmail.com>
2026-08-17 23:01:18 +0530

    added find the  largest and smallest DSA problem without using sorted()

---
+++ 07_find_largest_number.py
@@ -0,0 +1,18 @@
+def find_largest(nums): # for find the largest number
+    largest = nums[0]
+    for num in nums:
+        if num > largest:
+            largest = num
+    return largest
+        
+print(find_largest([3, 7, 2, 9, 4]))  # should print 9
+
+def find_smallest(nums): # for find the smallest number
+    smallest = nums[0]
+
+    for s in nums:
+        if s < smallest:
+            smallest = s
+    return smallest
+
+print(find_smallest([3,7,2,9,4]))
